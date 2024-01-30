907d02412cc947de37c6384ccc872f5ed3819b21
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-30 17:59:21 -0600

    Chore. Eneable import & export syntax

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
 // Definir puerto
 const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {  //req = lo que enviamos - res= lo que express nos responde
+    res.send('Inicio');
+});
+
+app.get('/nosotros', (req, res) => {  //req = lo que enviamos - res= lo que express nos responde
+    res.send('Nosotros');
+});
+
+app.get('/contacto', (req, res) => {  //req = lo que enviamos - res= lo que express nos responde
+    res.send('Contacto');
+});
 
 app.listen(port, () => {
     console.log(`El servidor está funcionando en el puerto ${port}`);
