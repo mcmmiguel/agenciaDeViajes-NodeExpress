275d7ad954d2aad04c515dbdade2d302111d0df7
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-30 18:03:54 -0600

    Feat. Add router

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 import express from 'express';
+import router from './routes/index.js';
 
 const app = express();
 
 // Definir puerto
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {  //req = lo que enviamos - res= lo que express nos responde
-    res.send('Inicio');
-});
-
-app.get('/nosotros', (req, res) => {  //req = lo que enviamos - res= lo que express nos responde
-    res.send('Nosotros');
-});
-
-app.get('/contacto', (req, res) => {  //req = lo que enviamos - res= lo que express nos responde
-    res.send('Contacto');
-});
+// Agregar Router
+app.use('/', router);
 
 app.listen(port, () => {
     console.log(`El servidor está funcionando en el puerto ${port}`);
