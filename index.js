02481a734705e6181f40494411aef4bcfef52689
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-30 19:22:59 -0600

    Feat. Show js local variables with pug

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const port = process.env.PORT || 4000;
 // Habilitar PUG
 app.set('view engine', 'pug');
 
+// Obtener el año actual
+app.use((req, res, next) => {
+    const year = new Date();
+
+    res.locals.currentYear = year.getFullYear();
+    next();
+})
+
 // Definir la carpeta publica
 app.use(express.static('public'));
 
