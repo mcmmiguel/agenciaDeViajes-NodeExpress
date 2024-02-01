f0a4821f32026c2d2dcc7c8c2fda33d6ebba1ab1
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-31 19:36:09 -0600

    Feat. Consult db of each travel URL

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -16,7 +16,6 @@ const paginaViajes = async (req, res) => {  //req = lo que enviamos - res= lo qu
 
     // Consultar base de datos
     const viajes = await Viaje.findAll();
-    console.log(viajes);
 
     res.render('viajes', {
         pagina: 'Próximos viajes',
@@ -30,9 +29,27 @@ const paginaTestimoniales = (req, res) => {  //req = lo que enviamos - res= lo q
     });
 }
 
+// Muestra un viaje por su slug
+const paginaDetalleViaje = async (req, res) => {
+
+    const { slug } = req.params;
+
+    try {
+        const viaje = await Viaje.findOne({ where: { slug } });
+
+        res.render('viaje', {
+            pagina: 'Información Viaje',
+            viaje
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 export {
     paginaInicio,
     paginaNosotros,
     paginaViajes,
-    paginaTestimoniales
+    paginaTestimoniales,
+    paginaDetalleViaje
 }
