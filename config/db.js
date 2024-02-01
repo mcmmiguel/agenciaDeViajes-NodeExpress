9613d71cd81410eedc5199420d4937ab6be407f3
Miguel Cobian <miguelcobian99@gmail.com>
2024-01-31 22:06:23 -0600

    Chore. Set up database host

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
 dotenv.config()
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    port: '3306',
-    dialect: 'mysql',
+const db = new Sequelize(process.env.DATABASE_URL, {
     define: {
         timestamps: false,
     },
